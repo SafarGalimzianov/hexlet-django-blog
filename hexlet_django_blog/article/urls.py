d7501f3b0bbea872de002db47dd3cbea1606e8a8
Safar Galimzianov <safargalimzianov@protonmail.com>
2024-12-15 13:57:17 +0500

    8.1. Routing to specific pages

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from hexlet_django_blog.article import views
 
 urlpatterns = [
-    path('', views.ArticlePageView.as_view()),
+    path('', views.ArticlePageView.as_view(), name='articles'),
+    path('<str:tag>/<int:article_id>', views.ArticlePageView.as_view(), name='article')
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -4,11 +4,20 @@ from django.shortcuts import render
 class ArticlePageView(View):
         
     items = [
-        {'author': 'J.K. Rowling', 'work': 'Harry Potter', 'date': '1982-04-03'},
-        {'author': 'Eliezer Judkowskiy', 'work': 'Harry Potter but Rationalised', 'date': '2011-10-17'},
+        {'article_id': 1, 'tag': 'fiction', 'author': 'J.K. Rowling', 'work': 'Harry Potter', 'date': '1982-04-03'},
+        {'article_id': 2, 'tag': 'fiction', 'author': 'Eliezer Judkowskiy', 'work': 'Harry Potter but Rationalised', 'date': '2011-10-17'},
     ]
 
-    template = 'article/index.html'
+    template_articles = 'article/index.html'
+    template_one_article = 'article/one_article.html'
     
-    def get(self, request, *args, **kwargs):
-        return render(request, template_name=self.template, context={'items': self.items})
+    def get_item(self, article_id, tag):
+        for item in self.items:
+            if item['article_id'] == article_id and item['tag'] == tag:
+                return item
+        raise ValueError('Article not found')
+
+    def get(self, request, article_id=None, tag=None, *args, **kwargs):
+        if article_id and tag:
+            return render(request, template_name=self.template_one_article, context={'item': self.get_item(article_id, tag)})
+        return render(request, template_name=self.template_articles, context={'items': self.items})
