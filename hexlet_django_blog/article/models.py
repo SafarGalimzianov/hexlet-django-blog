812ab4fd5c52e54952b1577aa30d9caa0eb6e9ff
Safar Galimzianov <safargalimzianov@protonmail.com>
2024-12-18 23:08:10 +0500

    9. Article Model migration

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# базовая модель для наследования
+class Article(models.Model):
+    # Атрибут - столбец таблицы
+    name = models.CharField(max_length=200) # VARCHAR(200)
+    body = models.TextField() #TEXT
+    # Незименяемая временная метка
+    created_at = models.DateTimeField(auto_now_add=True) # Не меняется после создания (CREATE)
+    # Изменяемая временная метка
+    updated_at = models.DateTimeField(auto_now=True) # Меняется после CREATE (UPDATE и при сохранении модели)
